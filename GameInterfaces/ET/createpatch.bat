88127dad5160de1cd1b256bd55d12962865526e4
jswigart <jswigart@cb4c95b2-fdc8-0eab-288b-8dca173e5f5f>
2010-09-12 23:38:55 +0000

    single shared property sheet for all projects

--- GameInterfaces/ET/createpatch.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Creating patch file...
-diff -urN --exclude=.svn --exclude=debug --exclude=release --exclude=Debug --exclude=Release --exclude=*.ncb --exclude=*.suo ET_SDK 2.60 > patches\omni-bot.patch
-echo done.
-pause
