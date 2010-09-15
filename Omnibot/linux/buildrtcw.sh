b17e26a7030e5483e2596b8dffeb27831f506ce6
ken.nickel <ken.nickel@cb4c95b2-fdc8-0eab-288b-8dca173e5f5f>
2010-09-15 03:51:26 +0000

    bleh, fixed game paths

--- Omnibot/linux/buildrtcw.sh
+++ Omnibot/linux/buildrtcw.sh
@@ -3,6 +3,6 @@
 export BOOST_BUILD_PATH=/boost/boost_1_44_0/tools/build/v2
 export BOOST=/boost/boost_1_44_0
 
-cd /mnt/omnibot_svn_root/GameInterfaces/RTCW/MP/WolfMP-Source/src/
+cd /mnt/omnibot_svn_root/GameInterfaces/RTCW/src/
 
 bjam -q release
