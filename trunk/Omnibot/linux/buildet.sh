ef24b0fb73ca48db0bc8a0e6318d43eab992fabe
ken.nickel <ken.nickel@cb4c95b2-fdc8-0eab-288b-8dca173e5f5f>
2010-09-15 03:51:26 +0000

    bleh, fixed game paths

--- trunk/Omnibot/linux/buildet.sh
+++ trunk/Omnibot/linux/buildet.sh
@@ -3,6 +3,6 @@
 export BOOST_BUILD_PATH=/boost/boost_1_44_0/tools/build/v2
 export BOOST=/boost/boost_1_44_0
 
-cd /mnt/omnibot_svn_root/GameInterfaces/ET/2.60/src/
+cd /mnt/omnibot_svn_root/GameInterfaces/ET/src/
 
 bjam -q release
