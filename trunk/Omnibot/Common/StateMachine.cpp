d1681c3d884f37104666289b307690436a81132b
ken.nickel <ken.nickel@cb4c95b2-fdc8-0eab-288b-8dca173e5f5f>
2011-12-09 00:08:29 +0000

    skyrim is rotting my brain. disabled uninterruptible paththrough until i consume a few more dragon souls

--- trunk/Omnibot/Common/StateMachine.cpp
+++ trunk/Omnibot/Common/StateMachine.cpp
@@ -319,7 +319,7 @@ void State::RootUpdate()
 obReal State::InternalGetPriority()
 {
 	// cs: hack to make sure paththrough isn't interrupted
-	if( (m_LastPriorityTime < IGame::GetTime() && !GetClient()->CheckUserFlag(Client::FL_UNINTERRUPTIBLE)) )
+	if( (m_LastPriorityTime < IGame::GetTime()) /*&& !GetClient()->CheckUserFlag(Client::FL_UNINTERRUPTIBLE)*/ )
 	{
 		const noSelectReason_t rsn = CanBeSelected();
 		SetSelectable( rsn == NoSelectReasonNone );
