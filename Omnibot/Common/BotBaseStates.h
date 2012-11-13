1024512ae92413fce6163aace21fdcc69138b8da
plastovicka <plastovicka@cb4c95b2-fdc8-0eab-288b-8dca173e5f5f>
2012-11-13 15:58:50 +0000

    fixed jumplow did not work at tank barrier or pile of box crates

--- Omnibot/Common/BotBaseStates.h
+++ Omnibot/Common/BotBaseStates.h
@@ -254,6 +254,7 @@ namespace AiState
 		Vector3f	m_PtOnPath;
 		Vector3f	m_LookAheadPt;
 		int			m_LadderDirection;
+		obint32		m_JumpTime;
 		
 		obuint32	m_PassThroughState;
 		int			m_PathThroughPtIndex;
