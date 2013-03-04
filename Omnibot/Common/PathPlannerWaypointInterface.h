98221b0c276252000f82b08a9523b97a4168c306
jswigart@gmail.com <jswigart@gmail.com@cb4c95b2-fdc8-0eab-288b-8dca173e5f5f>
2013-03-04 06:01:03 +0000

    missing file

---
+++ Omnibot/Common/PathPlannerWaypointInterface.h
@@ -0,0 +1,27 @@
+////////////////////////////////////////////////////////////////////////////////
+// 
+// $LastChangedBy: plastovicka $
+// $LastChangedDate: 2012-07-14 19:38:48 -0500 (Sat, 14 Jul 2012) $
+// $LastChangedRevision: 684 $
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __PATHPLANNERWAYPOINTINTERFACE_H__
+#define __PATHPLANNERWAYPOINTINTERFACE_H__
+
+class Waypoint;
+
+class PathPlannerWaypointInterface
+{
+public:
+	typedef enum
+	{
+		B_PATH_OPEN,
+		B_PATH_CLOSED,
+		B_INVALID_FLAGS
+	} BlockableStatus;
+
+	virtual BlockableStatus WaypointPathCheck(const Waypoint*, const Waypoint*, bool _draw) = 0;
+};
+
+#endif
