2b8c99a7ba1eb24b77c2543d44c3c579926d2403
plastovicka <plastovicka@cb4c95b2-fdc8-0eab-288b-8dca173e5f5f>
2013-11-20 15:48:42 +0000

    VS2013 compile fix

--- branches/stable/0.83/GameInterfaces/ET/src/cgame/Omnibot_Interprocess.cpp
+++ branches/stable/0.83/GameInterfaces/ET/src/cgame/Omnibot_Interprocess.cpp
@@ -1,3 +1,6 @@
+#define BOOST_NO_RVALUE_REFERENCES 1 // cs: < 1_45 vs2010 compile 'fix'
+#include <boost/interprocess/ipc/message_queue.hpp>
+
 #include "../game/q_shared.h"
 #include "Omnibot_Interprocess.h"
 #include "Omni-Bot_Types.h"
@@ -11,8 +14,6 @@ extern "C" void DrawDebugPolygon(vec3_t *verts, int _numverts, int _duration, in
 extern "C" void DrawDebugText(float *_start, const char *_msg, int _duration, int _color);
 //////////////////////////////////////////////////////////////////////////
 
-#define BOOST_NO_RVALUE_REFERENCES 1 // cs: < 1_45 vs2010 compile 'fix'
-#include <boost/interprocess/ipc/message_queue.hpp>
 using namespace boost::interprocess;
 
 extern "C" void OmnibotDisableDrawing();
