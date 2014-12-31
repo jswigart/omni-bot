4f34caab6cd0c0f6c7997945d669872e8dee2bee
plastovicka <plastovicka@cb4c95b2-fdc8-0eab-288b-8dca173e5f5f>
2014-12-31 19:26:46 +0000

    debug binaries link debug Boost libraries

--- 0.83/Omnibot/linux/buildbot_debug.sh
+++ 0.83/Omnibot/linux/buildbot_debug.sh
@@ -3,7 +3,7 @@
 export BOOST=/boost/boost_1_44_0
 export BOOST_BUILD_PATH=$BOOST/tools/build/v2
 export BOOST_LIB=$BOOST/stage/lib
-export BOOST_SUFFIX=-gcc41-mt-1_44
+export BOOST_SUFFIX=-gcc41-mt-d-1_44
 
 cd /media/sf_omnibot_svn_root/Omnibot
 $BOOST/bjam -q debug
