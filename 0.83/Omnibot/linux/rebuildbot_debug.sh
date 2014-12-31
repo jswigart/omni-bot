e798d89cb21780ec05c79bf756ebd2567ab88cf8
plastovicka <plastovicka@cb4c95b2-fdc8-0eab-288b-8dca173e5f5f>
2014-12-31 18:15:27 +0000

    BOOST_LIB and BOOST_SUFFIX variables in Linux scripts

--- 0.83/Omnibot/linux/rebuildbot_debug.sh
+++ 0.83/Omnibot/linux/rebuildbot_debug.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-export BOOST_BUILD_PATH=/boost/boost_1_44_0/tools/build/v2
 export BOOST=/boost/boost_1_44_0
+export BOOST_BUILD_PATH=$BOOST/tools/build/v2
+export BOOST_LIB=$BOOST/stage/lib
+export BOOST_SUFFIX=-gcc41-mt-1_44
 
-cd /mnt/omnibot_svn_root/Omnibot
-bjam -a -q debug
+cd /media/sf_omnibot_svn_root/Omnibot
+$BOOST/bjam -a -q debug
