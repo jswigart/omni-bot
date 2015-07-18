7908ab20fb0aa5251bbf0e6fcb5a1e5125366d6d
Petr Laštovička <plastovicka@users.noreply.github.com>
2015-07-18 15:29:00 +0200

    fixed current goal was instantly aborted if LimitTo was used inside Enter

--- 0.83/Omnibot/Common/StateMachine.cpp
+++ 0.83/Omnibot/Common/StateMachine.cpp
@@ -744,7 +744,7 @@ void State::LimitTo(const gmVariable &varThis, gmGCRoot<gmFunctionObject> &_fn, 
 	m_LimitCallback.m_LimitTo = _fn;
 	m_LimitCallback.m_Delay = _delay;
 	m_LimitCallback.m_OnlyWhenActive = _onlywhenactive;
-	m_LimitCallback.m_NextCallback = IGame::GetTime() + m_LimitCallback.m_Delay;
+	m_LimitCallback.m_NextCallback = 0;
 }
 
 void State::ClearLimitTo()
