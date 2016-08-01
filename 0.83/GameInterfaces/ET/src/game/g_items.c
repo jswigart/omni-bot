27ae968ee1adbd1e60651323e0400281c66c056a
Petr Laštovička <plastovicka@users.noreply.github.com>
2016-08-01 15:14:10 +0200

    fixed function GetEntityOwner returned dead player after gold was dropped and taken by another player

--- 0.83/GameInterfaces/ET/src/game/g_items.c
+++ 0.83/GameInterfaces/ET/src/game/g_items.c
@@ -943,6 +943,7 @@ gentity_t *LaunchItem( gitem_t *item, vec3_t origin, vec3_t velocity, int ownerN
 		gentity_t* flag = &g_entities[ g_entities[ownerNum].client->flagParent ];
 
 		dropped->s.otherEntityNum =	g_entities[ownerNum].client->flagParent;	// store the entitynum of our original flag spawner
+		g_entities[ownerNum].client->flagParent = 0;
 		dropped->s.density = 1;
 		dropped->think = Team_DroppedFlagThink;
 		dropped->nextthink = level.time + 30000;
